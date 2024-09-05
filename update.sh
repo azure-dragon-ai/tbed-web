68cc4af763ecd6d57c2e486613b7b63345381d5d
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-09-05 17:17:59 +0800

    update

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git push -u origin ai
 #npm run build
 rm -rf ../hugai-chatgpt-ui/leona-web/webroot/admin1/*
 cp -r dist/* ../hugai-chatgpt-ui/leona-web/webroot/admin1
-cd ../leona-web
+cd ../hugai-chatgpt-ui/leona-web
 git checkout main
 git add .
 git commit -a -m 'update'
